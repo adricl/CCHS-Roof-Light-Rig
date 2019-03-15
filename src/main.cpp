8dd03fee9c89f10d5661abc6d9a47090b7e49db6
Adric <adric.landman@gmail.com>
2019-03-16 00:01:00 +1100

    Colour will now change as position changes

--- src/main.cpp
+++ src/main.cpp
@@ -24,90 +24,86 @@ NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 CRGB leds[NUM_LEDS];
 
 VL53L1X sensor;
-int pos = -1;
 
-void calcPosUltrasonic(){
+int calcPosUltrasonic(){
   long dist = sonar.ping_cm();
   Serial.print("Distance: ");
   Serial.print(dist);
   Serial.println("cm ");
   if (dist > DIST_SENSOR_TO_LED && dist < MAX_DISTANCE) {
       int ledCount = (int)((dist - DIST_SENSOR_TO_LED)  / (100 / NUM_LEDS_PER_METER));
-      pos = NUM_LEDS - ledCount;
       Serial.print("Pos: ");
-      Serial.print(pos);
+      Serial.print(NUM_LEDS - ledCount);
       Serial.print(" ledCount: ");
       Serial.println(ledCount);
+      return NUM_LEDS - ledCount;
 
   }
   else {
-      pos = -1;
+      return -1;
   }
 }
 
-void calcPosTimeOfFlight(){
+int calcPosTimeOfFlight(){
   sensor.read();
-    Serial.print("range: ");
-    Serial.print(sensor.ranging_data.range_mm/10);
-    Serial.print("\tstatus: ");
-    Serial.print(VL53L1X::rangeStatusToString(sensor.ranging_data.range_status));
-    Serial.print("\tpeak signal: ");
-    Serial.print(sensor.ranging_data.peak_signal_count_rate_MCPS);
-    Serial.print("\tambient: ");
-    Serial.println(sensor.ranging_data.ambient_count_rate_MCPS);
+  Serial.print("range: ");
+  Serial.print(sensor.ranging_data.range_mm/10);
+  Serial.print("\tstatus: ");
+  Serial.print(VL53L1X::rangeStatusToString(sensor.ranging_data.range_status));
+  Serial.print("\tpeak signal: ");
+  Serial.print(sensor.ranging_data.peak_signal_count_rate_MCPS);
+  Serial.print("\tambient: ");
+  Serial.println(sensor.ranging_data.ambient_count_rate_MCPS);
 
-
-  //Range Status 0 is range Valid
-  //if (sensor.ranging_data.range_status ==  sensor.RangeValid){
-    //Convert to cm
-    uint16_t dist = sensor.ranging_data.range_mm / 10;
-    int ledCount = (int)(dist  / (100 / NUM_LEDS_PER_METER));
-    pos = ledCount;
-    Serial.print("Pos: ");
-    Serial.print(pos);
-    Serial.print(" ledCount: ");
-    Serial.print(ledCount);
-    Serial.print(" distance ");
-    Serial.println(dist);
-  //}
+  //Convert to cm
+  uint16_t dist = sensor.ranging_data.range_mm / 10;
+  int pos = (int)(dist  / (100 / NUM_LEDS_PER_METER));
+  Serial.print("Pos: ");
+  Serial.print(pos);
+  Serial.print(" distance ");
+  Serial.println(dist);
+  return pos;
 }
 
 void setup() {
   Serial.begin(115200);
+
+  //Setup Fast Led
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(255);
   FastLED.setDither( 255 );
 
+  //Setup Wire for I2C for Time of Flight Sensor
   Wire.begin();
   Wire.setClock(400000); // use 400 kHz I2C
-
   sensor.setTimeout(500);
   if (!sensor.init())
   {
     Serial.println("Failed to detect and initialize sensor!");
     while (1);
   }
-  
   sensor.setDistanceMode(VL53L1X::Long);
   sensor.setMeasurementTimingBudget(100000);
-
   sensor.startContinuous(50);
 }
  
 void loop() {
   //calcPosUltraSonic();
-  calcPosTimeOfFlight();
+  int pos = calcPosTimeOfFlight();
   if (pos > 0){
-    //Serial.println(pos);
     for(int i = 0; i < 5; i++){
-        leds[pos + i] = CRGB::Blue;    
+      if (pos + 1 <= NUM_LEDS){
+        leds[pos + i] = CHSV(pos, 255, 255);    
+      }
     }
   }
   
   FastLED.show();
   delay(100);
 
-  for(int i = 0; i < 5; i++){
-    leds[pos + i] = CRGB::Black;    
-  }
+    for(int i = 0; i < 5; i++){
+      if (pos + 1 <= NUM_LEDS){
+        leds[pos + i] = CRGB::Black;    
+      }
+    }
 }
