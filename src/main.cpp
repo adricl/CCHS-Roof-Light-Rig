40db0ee6530a8f2985682eef7a0416992861c0bd
Adric <adric.landman@gmail.com>
2024-09-17 23:07:59 +1000

    Fix sensitivity issues
    Will now display rainbow when not detecting anything.
    
    Fixes delay in sensor readings by adding a delay into pos movement when detection threshold is too low.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 
 #define DIST_SENSOR_TO_LED  10
 
+
 //LED
 #define NUM_LEDS              170
 #define NUM_LEDS_PER_METER    30
@@ -24,7 +25,9 @@
 CRGB leds[NUM_LEDS];
 VL53L1X sensor;
 int posLast = 0;
-int falsePosCount = 0;
+int noDataCount = 0;
+int noDataCountThreshold = 20;
+int prevPos = 0;
 SimpleKalmanFilter simpleKalmanFilter(5, 5, 1);
 
 int calcPosTimeOfFlight(){
@@ -40,19 +43,26 @@ int calcPosTimeOfFlight(){
   Serial.println(sensor.ranging_data.ambient_count_rate_MCPS);
 
   //Convert to cm
-  uint16_t dist = sensor.ranging_data.range_mm / 10;
-  float estimate = simpleKalmanFilter.updateEstimate(dist);
-  int pos = (int)((estimate + DIST_SENSOR_TO_LED)  / (100 / NUM_LEDS_PER_METER));
-  Serial.print("Pos: ");
-  Serial.print(pos);
-  Serial.print(" distance ");
-  Serial.println(dist);
   if (sensor.ranging_data.peak_signal_count_rate_MCPS > .4){
+    uint16_t dist = sensor.ranging_data.range_mm / 10;
+    float estimate = simpleKalmanFilter.updateEstimate(dist);
+    int pos = (int)((estimate + DIST_SENSOR_TO_LED)  / (100 / NUM_LEDS_PER_METER));
+    
+    Serial.print("Pos: ");
+    Serial.print(pos);
+    Serial.print(" distance ");
+    Serial.println(dist);
+  
+    noDataCount = 0;
+    prevPos = pos;
     return pos;
   }
-  else {
-    return -1;
+  else if (noDataCount < noDataCountThreshold) {
+    noDataCount++;
+    return prevPos;
   }
+
+  return -1;
   
 }
 
@@ -93,6 +103,7 @@ void loop() {
   int currPos = calcPosTimeOfFlight();
   int posDiffSize = abs(currPos - posLast) + 15;
   static byte colour;
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
   colour += 1;
 
   int pos = int((posLast + currPos)/2);
@@ -117,7 +128,50 @@ void loop() {
     }
   }
   else {
-    fill_solid(leds, NUM_LEDS, CRGB::Black);
+    
+    pride();
     FastLED.show();
+    
+  }
+}
+
+void pride() 
+{
+  static uint16_t sPseudotime = 0;
+  static uint16_t sLastMillis = 0;
+  static uint16_t sHue16 = 0;
+ 
+  uint8_t sat8 = beatsin88( 87, 220, 250);
+  uint8_t brightdepth = beatsin88( 341, 96, 224);
+  uint16_t brightnessthetainc16 = beatsin88( 203, (25 * 256), (40 * 256));
+  uint8_t msmultiplier = beatsin88(147, 23, 60);
+ 
+  uint16_t hue16 = sHue16;//gHue * 256;
+  uint16_t hueinc16 = beatsin88(113, 1, 3000);
+  
+  uint16_t ms = millis();
+  uint16_t deltams = ms - sLastMillis ;
+  sLastMillis  = ms;
+  sPseudotime += deltams * msmultiplier;
+  sHue16 += deltams * beatsin88( 400, 5,9);
+  uint16_t brightnesstheta16 = sPseudotime;
+  
+  for( uint16_t i = 0 ; i < NUM_LEDS; i++) {
+    hue16 += hueinc16;
+    uint8_t hue8 = hue16 / 256;
+ 
+    brightnesstheta16  += brightnessthetainc16;
+    uint16_t b16 = sin16( brightnesstheta16  ) + 32768;
+ 
+    uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
+    uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
+    bri8 += (255 - brightdepth);
+    
+    CRGB newcolor = CHSV( hue8, sat8, bri8);
+    
+    uint16_t pixelnumber = i;
+    pixelnumber = (NUM_LEDS-1) - pixelnumber;
+    
+    nblend( leds[pixelnumber], newcolor, 64);
   }
 }
